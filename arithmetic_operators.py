8f059260152beefc6e9652820c9f11517b63a9ba
Manuel Otero <manuel.otero@cooleche.com>
2022-10-07 20:38:53 -0500

    data frame, node swap

---
+++ arithmetic_operators.py
@@ -0,0 +1,5 @@
+print(3 + 5) # 8
+print(1 + 2 + 3 * 3) # 12
+print(3 ** 3) # 9
+print(9 % 2) # 1
+print(5//2)
--- node.py
+++ node.py
@@ -3,19 +3,20 @@
 # Queue the first-in first-out (FIFO)
 
 # Node
+# Linked list
 class Node:
-  def __init__(self, value, link_node=None):
+  def __init__(self,value,next_node=None):
     self.value = value
-    self.link_node = link_node
-    
-  def set_link_node(self, link_node):
-    self.link_node = link_node
-    
-  def get_link_node(self):
-    return self.link_node
-  
+    self.next_node = next_node
+
   def get_value(self):
     return self.value
+
+  def get_next_node(self):
+    return self.next_node
+
+  def set_next_node(self,next_node):
+    self.next_node = next_node
 
 yacko = Node("likes to yak")
 wacko = Node("has a penchant for hoarding snacks")
